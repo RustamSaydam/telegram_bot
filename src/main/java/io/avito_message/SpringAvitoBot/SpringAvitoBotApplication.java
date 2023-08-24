46218a70f7a3c93756c03c7bd93e1768c752cbc9
RustamSaydam <rustamsaydam@icloud.com>
2023-08-24 19:30:48 +0300

    1.0 First start Telegram Bot

---
+++ src/main/java/io/avito_message/SpringAvitoBot/SpringAvitoBotApplication.java
@@ -0,0 +1,13 @@
+package io.avito_message.SpringAvitoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAvitoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAvitoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/avito_message/SpringAvitoBot/SpringAvitoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.avito_message.SpringAvitoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAvitoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
